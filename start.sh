d6d7b17e90b0d1ff55519737a1e69ac90429f6b2
Oq4Tr6 <120112189+Oq4Tr6@users.noreply.github.com>
2022-12-27 13:22:14 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 #请修改为自己的UUID
 export UUID=b9db2cc8-538c-4ae8-9245-274a02335a12
 
-#请修改为自己设置的伪装站，不要带https://github.com/technext/Design4Profit/archive/refs/heads/master.zip
+#请修改为自己设置的伪装站，不要带https://github.com/technext/Flusk/archive/refs/heads/master.zip
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
